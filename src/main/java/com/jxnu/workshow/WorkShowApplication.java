c827b29d540f8d1e7dcd53e71cb42ac225a20c02
coriander <1019958984@qq.com>
2020-08-10 23:06:37 +0800

    init

---
+++ src/main/java/com/jxnu/workshow/WorkShowApplication.java
@@ -0,0 +1,16 @@
+package com.jxnu.workshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 10199
+ */
+@SpringBootApplication
+public class WorkShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkShowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jxnu/workshow/WorkShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxnu.workshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
